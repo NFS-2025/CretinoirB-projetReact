acc0e224b3ee0cf7632849fe262ff13b3d3ce25e
Bcretinoir <benjamin.cretinoir@hotmail.fr>
2025-04-16 15:00:37 +0200

    navbar ?

--- projetreact/src/pages/Navigation.tsx
+++ projetreact/src/pages/Navigation.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import "../pages/NavBar.css";
 
 const Navigation: React.FC = () => {
-    return (
-        <nav className="navbar">
-            <div className="logo">
-                <h2>Pokédex</h2>
-            </div>
-            <ul className="nav-links">
-                <li>
-                    <Link to="/" className="nav-link">Pokémon List</Link>
-                </li>
-                <li>
-                    <Link to="/pokemonList" className="nav-link">PokémonList</Link>
-                </li>
-                <li>
-                    <Link to="pokeList" className="nav-link">List</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <h2>Pokédex</h2>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/" className="nav-link">
+            Pokémon List
+          </Link>
+        </li>
+        <li>
+          <Link to="/pokemonList" className="nav-link">
+            PokémonList
+          </Link>
+        </li>
+        <li>
+          <Link to="pokeList" className="nav-link">
+            List
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
