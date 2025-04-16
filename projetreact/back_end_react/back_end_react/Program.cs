3f3ad2524435a79a2da6ee3335a1071b85d93707
Bcretinoir <benjamin.cretinoir@hotmail.fr>
2025-04-16 15:34:43 +0200

    Open bar biiiiitch

--- projetreact/back_end_react/back_end_react/Program.cs
+++ projetreact/back_end_react/back_end_react/Program.cs
@@ -1,14 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Ajouter CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigin", policy =>
+    options.AddPolicy("AllowAnyOrigin", policy =>
     {
-        // Ajouter les deux URLs de ton front-end
-        policy.WithOrigins("https://exemplary-nourishment-frontreact-dafe.up.railway.app", "https://exemplary-nourishment-frontreact.up.railway.app")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy.AllowAnyOrigin()   
+              .AllowAnyHeader()  
+              .AllowAnyMethod();  
     });
 });
 
@@ -26,15 +23,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 );
 
 var app = builder.Build();
-
-// Appliquer CORS pour tous les environnements
-app.UseCors("AllowSpecificOrigin");
+app.UseCors("AllowAnyOrigin");
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-
-    // Ajouter Swagger UI
     app.UseSwagger();
     app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "API Backend React v1"); });
 }
