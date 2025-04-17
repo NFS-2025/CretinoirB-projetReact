f3b85e8309157ece76f76ec35c542d478e11d4e3
Bcretinoir <benjamin.cretinoir@hotmail.fr>
2025-04-17 11:05:08 +0200

    fix back 2

--- projetreact/back_end_react/back_end_react/Program.cs
+++ projetreact/back_end_react/back_end_react/Program.cs
@@ -1,13 +1,16 @@
-using back_end_react.WebApi.Form;
-using back_end_react.WebApi.IService;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-
-
-
+using back_end_react.WebApi.Form;                
+using back_end_react.WebApi.IService;              
+using back_end_react.WebApi.Service;              
+using back_end_react.WebApi.Repository;            
+using back_end_react.WebApi.IRepository;          
+using back_end_react.WebApi.Infrastructure;        
+using Microsoft.AspNetCore.Mvc;                    
+using Microsoft.EntityFrameworkCore;                
+using MySql.EntityFrameworkCore.Extensions;         
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuration de CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAnyOrigin", policy =>
@@ -18,6 +21,7 @@ builder.Services.AddCors(options =>
     });
 });
 
+// Ajout des services nécessaires
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,16 +29,14 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-  options.UseMySql(
-    "Server=localhost;Database=testdb;User=root;Password=;",
-    ServerVersion.AutoDetect("Server=localhost;Database=testdb;User=root;Password=;")
-  )
+    options.UseMySql(
+        "Server=localhost;Database=testdb;User=root;Password=;",
+        ServerVersion.AutoDetect("Server=localhost;Database=testdb;User=root;Password=;")
+    )
 );
 
 var app = builder.Build();
-
 app.UseCors("AllowAnyOrigin");
-
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -44,5 +46,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseRouting();
 app.MapControllers();
-
 app.Run();
